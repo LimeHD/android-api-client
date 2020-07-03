ab14b287b4cf57ff1a67002da22c19235999722a
lissyo <igorekrussia@gmail.com>
2020-07-03 11:02:28 +0300

    Улучшена ссылка на архив под новое требование из сессии

--- androidapimodule/src/main/java/tv/limehd/androidapimodule/LimeUri.java
+++ androidapimodule/src/main/java/tv/limehd/androidapimodule/LimeUri.java
@@ -10,6 +10,7 @@ public class LimeUri {
     private static String START_AT = "start_at";
     private static String FINISH_AT = "finish_at";
     private static String LOCALE = "locale";
+    private static String DURATION = "duration";
 
     private static String STREAM_URL = "${stream_id}";
 
@@ -19,7 +20,9 @@ public class LimeUri {
                 .authority(api_root)
                 .appendEncodedPath(endpoint_channels)
                 .appendQueryParameter(LOCALE, locale)
-                .appendEncodedPath(channel_group_id).build().toString();
+                .appendEncodedPath(channel_group_id)
+                .build()
+                .toString();
     }
 
     public static String getUriBroadcast(String scheme, String api_root, String endpoint_broadcast
@@ -31,25 +34,41 @@ public class LimeUri {
                 .appendQueryParameter(TIME_ZONE, time_zone)
                 .appendQueryParameter(START_AT, before_date)
                 .appendQueryParameter(LOCALE, locale)
-                .appendQueryParameter(FINISH_AT, after_date).build().toString();
+                .appendQueryParameter(FINISH_AT, after_date)
+                .build()
+                .toString();
     }
 
     public static String getUriSession(String scheme, String api_root, String endpoint_session) {
         return new Uri.Builder()
                 .scheme(scheme)
                 .authority(api_root)
-                .appendEncodedPath(endpoint_session).build().toString();
+                .appendEncodedPath(endpoint_session)
+                .build()
+                .toString();
     }
 
     public static String getUriPing(String scheme, String api_root, String endpoint_ping) {
         return new Uri.Builder()
                 .scheme(scheme)
                 .authority(api_root)
-                .appendEncodedPath(endpoint_ping).build().toString();
+                .appendEncodedPath(endpoint_ping)
+                .build()
+                .toString();
     }
 
     public static String getUriTranslation(String stream_url, String channel_id) {
         return stream_url.replace(STREAM_URL, channel_id);
+    }
+
+    public static String getArchiveUriTranslation(String archive_url, String channel_id, String start_at, String duration){
+        String archive_hls_url = archive_url.replace(STREAM_URL, channel_id);
+        return new Uri.Builder()
+                .appendEncodedPath(archive_hls_url)
+                .appendQueryParameter(START_AT, start_at)
+                .appendQueryParameter(DURATION, duration)
+                .build()
+                .toString();
     }
 
 }
